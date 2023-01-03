865eaa53fb42b884f7326c5f55aace21e3a82d7f
tquyen2109 <t.quyen1997@yahoo.com>
2023-01-03 15:09:33 +0700

    New Campaign Page and Header

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,38 @@
 import React, {Component} from "react";
 import factory from "../ethereum/factory";
+import {Card} from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css';
+import {Button} from 'semantic-ui-react'
+import Layout from "../components/Layout";
 
 class CampaignIndex extends Component {
     static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        return { campaigns };
+        return {campaigns};
+    }
+
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            }
+        });
+        return <Card.Group centered items={items}/>
     }
 
     render() {
-        return <div>{this.props.campaigns[0]}</div>
+        return (
+            <Layout>
+                <div>
+                    <h3>Open Campaigns</h3>
+                    <Button floated={"right"} content={"Create Campaign"} icon={"add circle"} primary></Button>
+                    {this.renderCampaigns()}
+                </div>
+            </Layout>
+        )
     }
 }
 
